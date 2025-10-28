39ca9d37a7edab85a2b810c4588a113d5ceaee72
weifco <limanviktoria08@gmail.com>
2025-10-28 23:47:45 +0300

    Add files via upload

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,54 @@
+﻿// Implementing a calculator in 
+// C# using switch statement.
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator_c_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value;
+            do
+            {
+                int res;
+                Console.Write("Введите первое число:");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите второе число:");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Введите символ(/,+,-,*):");
+                string symbol = Console.ReadLine();
+
+                switch (symbol)
+                {
+                    case "+":
+                        res = num1 + num2;
+                        Console.WriteLine("Addition:" + res);
+                        break;
+                    case "-":
+                        res = num1 - num2;
+                        Console.WriteLine("Subtraction:" + res);
+                        break;
+                    case "*":
+                        res = num1 * num2;
+                        Console.WriteLine("Multiplication:" + res);
+                        break;
+                    case "/":
+                        res = num1 / num2;
+                        Console.WriteLine("Division:" + res);
+                        break;
+                    default:
+                        Console.WriteLine("Wrong input");
+                        break;
+                }
+                Console.ReadLine();
+                Console.Write("Do you want to continue(y/n):");
+                value = Console.ReadLine();
+            }
+            while (value == "y" || value == "Y");
+        }
+    }
+
+}
